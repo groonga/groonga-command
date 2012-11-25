46c704e8758363f54858eee483f8269b51856359
Kouhei Sutou <kou@clear-code.com>
2012-11-25 16:58:42 +0900

    Require built-in libraries first

--- lib/groonga/command/select.rb
+++ lib/groonga/command/select.rb
@@ -16,10 +16,11 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "groonga/command/base"
 require "English"
 require "shellwords"
 require "cgi"
+
+require "groonga/command/base"
 
 module Groonga
   module Command
