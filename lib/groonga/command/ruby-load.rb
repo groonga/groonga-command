c72ff89f9bb7ad8dac2e0d2cd23903b7a8ac1146
Kouhei Sutou <kou@clear-code.com>
2013-10-14 23:06:30 +0900

    ruby_load: add

---
+++ lib/groonga/command/ruby-load.rb
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "groonga/command/base"
+
+module Groonga
+  module Command
+    # A command class that represents `ruby_load` command.
+    #
+    # @since 1.0.6
+    class RubyLoad < Base
+      Command.register("ruby_load", self)
+
+      class << self
+        def parameter_names
+          [
+            :path,
+          ]
+        end
+      end
+
+      # @return [String] `path` parameter value.
+      # @since 1.0.6
+      def path
+        self[:path]
+      end
+    end
+  end
+end
---
+++ test/command/test-ruby-load.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+class RubyLoadCommandTest < Test::Unit::TestCase
+  private
+  def ruby_load_command(pair_arguments={}, ordered_arguments=[])
+    Groonga::Command::RubyLoad.new("ruby_load",
+                                   pair_arguments,
+                                   ordered_arguments)
+  end
+
+  class ConstructorTest < self
+    def test_ordered_arguments
+      path = "my-library.rb"
+
+      ordered_arguments = [
+        path,
+      ]
+      command = ruby_load_command({}, ordered_arguments)
+      assert_equal({
+                     :path => path,
+                   },
+                   command.arguments)
+    end
+  end
+
+  class PathTest < self
+    def test_reader
+      command = ruby_load_command(:path => "my-library.rb")
+      assert_equal("my-library.rb", command.path)
+    end
+  end
+end
