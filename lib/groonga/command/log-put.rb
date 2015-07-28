3f1e5c2832f2177ba0365af182bc7dc95c7a6c0d
Kouhei Sutou <kou@clear-code.com>
2015-07-28 10:50:52 +0900

    Support log_put

---
+++ lib/groonga/command/log-put.rb
@@ -0,0 +1,51 @@
+# Copyright (C) 2015  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "groonga/command/base"
+
+module Groonga
+  module Command
+    # A command class that represents `log_put` command.
+    #
+    # @since 1.1.3
+    class LogPut < Base
+      Command.register("log_put", self)
+
+      class << self
+        def parameter_names
+          [
+            :level,
+            :message,
+          ]
+        end
+      end
+
+      # @return [String] `level` parameter value.
+      #
+      # @since 1.1.3
+      def level
+        self[:level]
+      end
+
+      # @return [String] `message` parameter value.
+      #
+      # @since 1.1.3
+      def message
+        self[:message]
+      end
+    end
+  end
+end
