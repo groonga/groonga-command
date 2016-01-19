7ec149db83c2e83c5cae2b8cb3393e52853050b4
Kouhei Sutou <kou@clear-code.com>
2016-01-19 23:55:58 +0900

    Bump version

--- lib/groonga/command/version.rb
+++ lib/groonga/command/version.rb
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-#
 # Copyright (C) 2012-2016  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
@@ -18,6 +16,6 @@
 
 module Groonga
   module Command
-    VERSION = "1.1.5"
+    VERSION = "1.1.6"
   end
 end
