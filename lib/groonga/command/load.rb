66733b9a1601bd3317d669a63eb3de8ae846f469
Sutou Kouhei <kou@clear-code.com>
2020-01-10 17:11:16 +0900

    Load#build_arrow_array: add support for raw value load

--- lib/groonga/command/load.rb
+++ lib/groonga/command/load.rb
@@ -84,7 +84,19 @@ module Groonga
         unless defined?(::Arrow)
           raise NotImplementedError, "red-arrow is required"
         end
-        builder = ArrowTableBuilder.new(columns, values)
+        source_lines = original_source.lines
+        if source_lines.size == 1
+          target_columns = columns
+          target_values = values
+        else
+          if /\s--columns\s/ =~ source_lines.first
+            target_columns = columns
+          else
+            target_columns = nil
+          end
+          target_values = JSON.parse(source_lines[1..-1].join(""))
+        end
+        builder = ArrowTableBuilder.new(target_columns, target_values)
         builder.build
       end
 
